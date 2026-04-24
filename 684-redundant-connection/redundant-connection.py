c5484dfa2758e7eae10998fbaa6cc75b8fec345a
Muhammad Ali <malitariq5324@gmail.com>
2026-04-24 06:12:06 +0500

    Time: 83 ms (5.07%) | Memory: 20.3 MB (12.01%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,29 +1,23 @@
-class Union:
-    def __init__(self,N):
-        self.parent = {i:i for i in range(1,N+1)}
-        self.rank = {i:0 for i in range(1,N+1)}
+from collections import defaultdict
+from typing import List
 
-    def findP(self,node):
-        if node == self.parent[node]: return node
-        self.parent[node] = self.findP(self.parent[node])
-        return self.parent[node]
-
-    def union(self,node1,node2):
-        p1,p2 = self.findP(node1),self.findP(node2)
-        if p1 == p2: return True
-        if self.rank[p1]>self.rank[p2]:
-            self.parent[p2] = p1
-        elif self.rank[p2]>self.rank[p1]:
-            self.parent[p1] = p2
-        else:
-            self.parent[p2] = p1
-            self.rank[p1]+=1
-        return False
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        N = len(edges)
-        DSU = Union(N)
-        for n1,n2 in edges: 
-            if DSU.union(n1,n2):
-                return [n1,n2]
+        ans = []
+        adjL = defaultdict(list)
+        def dfs(node, target, visit):
+            if node in visit: return False
+            visit.add(node)
+            for ne in adjL[node]:
+                if ne==target: return True
+                elif ne in visit: continue
+                if dfs(ne,target,visit): return True
+            return False
+        for x,y in edges:
+            visited = set()
+            if dfs(x,y,visited):
+                ans = [x, y]
+            adjL[x].append(y)
+            adjL[y].append(x)
+        return ans
