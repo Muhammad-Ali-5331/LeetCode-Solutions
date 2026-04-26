2c41863ac8fccb953426c90271030551bdb86cbe
Muhammad Ali <malitariq5324@gmail.com>
2026-04-26 18:22:39 +0500

    Time: 7 ms (45.74%) | Memory: 22.7 MB (5.12%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -9,9 +9,15 @@ void dfs(int node,vector<bool>&visited,stack<int>& st,unordered_map<int,vector<i
 }
 class Solution {
 public:
+    
     bool canFinish(int numCourses, vector<vector<int>>& connections) {
         unordered_map<int,vector<int>> adjList;
-        for (auto node:connections) { adjList[node[1]].push_back(node[0]); }
+        for (auto node:connections) {
+
+            // As node[1] is pre-req of node[0] so there is a directed edge from node[1] --> node[0]
+            
+            adjList[node[1]].push_back(node[0]);
+        }
         vector<int> res;
         vector<bool> visited(numCourses,false);
         stack<int> st;
@@ -20,6 +26,9 @@ public:
         unordered_map<int,int> MAP;
         for (int i = 0;i<res.size();i++) { MAP[res[i]] = i; }
         for (auto node:connections) {
+            // Every Dependent {v} here node[0] should appear after the pre-req {u} here node[1]
+
+            // Here there is an edge btw u --> v so every u should appear before v
             int uInd = MAP[node[1]];
             int vInd = MAP[node[0]];
             if (uInd>=vInd) return false;
