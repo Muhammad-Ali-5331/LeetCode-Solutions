de952d3558b1b674520456080e67fd03928a43f7
Muhammad Ali <malitariq5324@gmail.com>
2026-05-18 06:57:27 +0500

    Time: 11 ms (26.73%) | Memory: 34.3 MB (28.35%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -10,24 +10,23 @@ using namespace std;
 // };
 class Solution {
 public:
-    void pthSum(TreeNode* root, int currSum,vector<int> currV,int &TS, vector<vector<int>> &res) {
+    void pthSum(TreeNode* root,vector<int> currV,int TS, vector<vector<int>> &res) {
         if (root->left == nullptr and root->right == nullptr) {
-            if (currSum+root->val == TS) {
+            if (TS-root->val == 0) {
                 currV.push_back({root->val});
                 res.push_back(currV);
             }
         }
         else {
-            currSum+=root->val;
             currV.push_back({root->val});
-            if (root->left!=nullptr) pthSum(root->left,currSum,currV,TS,res);
-            if (root->right!=nullptr)pthSum(root->right,currSum,currV,TS,res);
+            if (root->left!=nullptr) pthSum(root->left,currV,TS-root->val,res);
+            if (root->right!=nullptr)pthSum(root->right,currV,TS-root->val,res);
         }
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> res;
         if (root == nullptr){return res;}
-        pthSum(root,0,{},targetSum,res);
+        pthSum(root,{},targetSum,res);
         return res;
     }
 };
