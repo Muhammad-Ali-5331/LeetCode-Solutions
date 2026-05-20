d5feda7a5bef22258cd38e7b1727084471852102
Muhammad Ali <malitariq5324@gmail.com>
2026-05-20 18:57:56 +0500

    Time: 1 ms (21.64%) | Memory: 19.3 MB (84.78%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -4,20 +4,16 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         ROWS,COLS = len(image),len(image[0])
         def isValid(r,c): return 0<=r<ROWS and 0<=c<COLS
-        visit = [[False]*COLS for _ in range(ROWS)]
         dirs = [(1,0),(-1,0),(0,-1),(0,1)]
         q = deque()
         if image[sr][sc]!=color:
             q.append((sr,sc))
+            start = image[sr][sc]
             while q:
                 curX,curY = q.pop()
-                if visit[curX][curY]: continue
-                visit[curX][curY] = True
-                curC = image[curX][curY]
                 image[curX][curY] = color
                 for x,y in dirs:
                     nX,nY = x+curX,y+curY
                     if not isValid(nX,nY): continue
-                    if visit[nX][nY]: continue
-                    if image[nX][nY] == curC: q.append((nX,nY))
+                    if image[nX][nY] == start: q.append((nX,nY))
         return image
