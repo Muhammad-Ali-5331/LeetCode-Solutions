e2cad1f9dd49ccf810a4c557b1a013b27827f539
Muhammad Ali <malitariq5324@gmail.com>
2026-06-16 08:37:39 +0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (56.75%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,8 +1,12 @@
-class Solution(object):
-    def removeElement(self, nums, val):
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
         k = 0
-        for i in range(len(nums)):
-            if (nums[i] != val):
+        i = 0
+        n = len(nums)
+        while i<n:
+            while i<n and nums[i] == val: i+=1
+            if i<n:
                 nums[k] = nums[i]
-                k += 1
+                k+=1
+            i+=1
         return k
