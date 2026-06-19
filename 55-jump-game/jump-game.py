063bbcf0772b51521302273ed10a46decf7548a7
Muhammad Ali <malitariq5324@gmail.com>
2026-06-20 00:44:55 +0500

    Time: 55 ms (15.43%) | Memory: 20.5 MB (17.90%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -2,11 +2,13 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
         if n == 1: return True
-        steps = nums[0]
+        dp = [0]*n
+        dp[0] = nums[0]
         i = 0
-        while steps:
+        while True:
             if i+1 == n: return True
             if i>n: break
-            steps = max(steps-1,nums[i])
+            dp[i] = max(dp[i-1 if i-1>=0 else 0]-1,nums[i])
+            if dp[i] == 0: break
             i+=1
         return False
