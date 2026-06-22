58253d4ff79d8473c24b632aca24bf6c896b37ab
Muhammad Ali <malitariq5324@gmail.com>
2026-06-22 10:39:36 +0500

    Time: 2 ms (68.66%) | Memory: 19.1 MB (99.41%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,11 +1,9 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         MAP = Counter(text)
-        count = 0
-        word = "balloon"
-        while True:
-            for ch in word:
-                if MAP.get(ch,0)>0:
-                    MAP[ch] = max(0,MAP.get(ch,0)-1)
-                else: return count
-            count+=1
+        bC = MAP.get('b',0)
+        aC = MAP.get('a',0)
+        nC = MAP.get('n',0)
+        lC = MAP.get('l',0)//2
+        oC = MAP.get('o',0)//2
+        return min(bC,aC,nC,lC,oC)
