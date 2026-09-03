73d96bb53301fa2c466990dfd2b455ab28ff2855
Muhammad Ali <malitariq5324@gmail.com>
2026-09-03 13:00:34 +0500

    Time: 175 ms (7.92%) | Memory: 35.5 MB (28.66%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def uniformArray(self, nums: list[int]) -> bool:
+        n = len(nums)
+        even,odd = 0,0
+        foundOdd,foundEven = False,False
+        minEven,minOdd = float("inf"),float("inf")
+        for i in range(n):
+            if nums[i]%2 == 0:
+                foundEven = True
+                minEven = min(minEven,nums[i])
+            else:
+                foundOdd = True
+                minOdd = min(minOdd,nums[i])
+        if not foundEven or not foundOdd: return True
+        
+        # -- Even Parity -- #
+        for i in range(n):
+            if nums[i]%2 == 0: even+=1
+            else:
+                if minOdd == nums[i]: break
+                even+=1
+        
+        # -- Odd Parity -- #
+        for i in range(n):
+            if nums[i]%2 != 0: odd+=1
+            elif nums[i]-minOdd>=1: odd+=1
+            else: break
+        return odd == n or even == n
